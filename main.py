630dcb333d042e621e9bd771fda2cbecee67ac5b
In24 <54269634+in24@users.noreply.github.com>
2022-03-15 12:43:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Izveidot klases: kvadrātam, taisnstūrim, trijstūrim.
+# Izveidot katrā klasē metodi, kura sevi nosauc
+# Ārpus klasēm ciklā ar vienu metodi izdrukāt visu daudzstūru nosaukumus un malu skaitu
+# Darbu iesniegt txt formātā
+print("Sveiki, VP3!")
