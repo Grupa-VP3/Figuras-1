b56da741ac659fbf3e732ebe1cd2611387368bc6
intagrabe <70010762+intagrabe@users.noreply.github.com>
2022-03-16 18:44:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 # Izveidot katrā klasē metodi, kura sevi nosauc
 # Ārpus klasēm ciklā ar vienu metodi izdrukāt visu daudzstūru nosaukumus un malu skaitu
 # Darbu iesniegt txt formātā
-print("Sveicināti,meitenes, VP3!")
+print("Sveicināti, VP3 meitenes!")
